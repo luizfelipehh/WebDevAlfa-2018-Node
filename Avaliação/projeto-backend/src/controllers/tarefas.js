192571bdcbebfd1f50e071099c83d62897404049
Luiz Felipe Heemann <lfheemann@hotmail.com>
2018-09-17 22:11:56 -0300

    Add files via upload

--- Avaliação/projeto-backend/src/controllers/tarefas.js
+++ Avaliação/projeto-backend/src/controllers/tarefas.js
@@ -1,30 +1,85 @@
+const {sequelize, Tarefa} = require('../models');
 
 function cadastro(request, response, next) {
-
+    Tarefa.create(request.body)
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Erro ao cadastrar Tarefa'));
 }
 
 function listagem(request, response, next) {
+    const Op = sequelize.Op;
+    let titulo = request.query.titulo;
+    if (titulo == null) {
+        titulo = '';
+    }
 
+    Tarefa.findAll({
+        where: {
+            titulo: {
+                [Op.like]: '%' + titulo + '%'
+            }
+        }
+    })
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Não foi encontrada a tarefa desejada'));
 }
 
-function buscaPorId(request, response, next) {
-
+function BuscaPorId(request, response, next) {
+    Tarefa.findOne({where: request.params})
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Tarefa não encontrada'));
 }
 
 function edicao(request, response, next) {
-
+    Tarefa.update(request.body, {
+        where: {
+            id: request.params.id
+        }
+    })
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Erro ao editar a tarefa'));
 }
 
 function remocao(request, response, next) {
-
+    Tarefa.destroy({
+        where: {
+            id: request.params.id
+        }
+    })
+            .then(result => (
+                        Tarefa.findAll({})
+                        .then(result => response.status(200).json(result))
+                        .catch(err => response.status(412).send('Tarefa não entrada para ser removida!'))
+                        ))
+            .catch(err => response.status(412).send('Erro ao excluir tarefa'));
 }
 
 function marcarConcluida(request, response, next) {
-
+    Tarefa.update(
+            {
+                concluida: 1
+            },
+            {
+                where: {
+                    id: request.params.id
+                }
+            })
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Erro para concluír tarefa'));
 }
 
 function desmarcarConcluida(request, response, next) {
-
+    Tarefa.update(
+            {
+                concluida: null
+            },
+            {
+                where: {
+                    id: request.params.id
+                }
+            })
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Erro ao desmarcar tarefa'));
 }
 
 module.exports = {
--- Avaliação/projeto-backend/src/controllers/usuarios.js
+++ Avaliação/projeto-backend/src/controllers/usuarios.js
@@ -1,18 +1,60 @@
+const {sequelize, Usuario} = require('../models');
+const {generateToken} = require("../utils/token");
 
 function cadastro(request, response, next) {
 
+    Usuario.create(request.body)
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Erro ao cadastrar usuário'));
+
 }
 
-function buscaPorId(request, response, next) {
-
+function BuscaPorId(request, response, next) {
+    Usuario.findOne({where: request.params})
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Usuário não encontrado!'));
 }
 
 function edicao(request, response, next) {
-
+    Usuario.update(request.body, {where: request.params})
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Erro ao editar Usuário!'));
 }
 
 function login(request, response, next) {
 
+    const {body} = request;
+    const {email, senha} = body;
+
+    Usuario.findOne({
+        where: {
+            email: email,
+            senha: senha
+        }
+    })
+            .then(result => {
+
+                if (!result) {
+                    response.status(412).send('Usuário ou Senha incorreta');
+                } else {                    
+                    const params = {
+                        nome: result.dataValues.nome,
+                        email: result.dataValues.email
+                    };
+                    
+                    const token = generateToken(params);
+                    response.status(200).json({
+                        token: token
+                    });
+                }
+
+            });
+}
+
+function getAll(request, response, next) { //recupera todos
+    Usuario.findAll({})
+            .then(result => response.status(200).json(result))
+            .catch(err => response.status(412).send('Usuário não encontrado'));
 }
 
 module.exports = {
@@ -20,4 +62,5 @@ module.exports = {
     buscaPorId,
     edicao,
     login,
+    getAll
 };
